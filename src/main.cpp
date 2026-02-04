707baa158e4d8c6035b114db95c5ddca5d26ac56
pcannon09 <pcannon775@gmail.com>
2026-02-04 20:13:31 +0100

    Testing to get the file time of the last modification for all 'cpp' files

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,17 @@
 
 #include "ciof/ciof.hpp"
 
+namespace
+{
+	std::string __timeToStr(const fsi::utils::TimeUtils_DateTime &time)
+	{
+		return ciof::format("%1/%2/%3 - %4:%5:%6.%7",
+				time.year, time.month, time.day,
+				time.hour, time.min, time.sec, time.ms
+		);
+	}
+}
+
 int main()
 {
 	fsi::Indexer indexer("main-indexer");
@@ -16,9 +27,9 @@ int main()
 		indexer.addExtendedInfo(info);
 	}
 
-	for (const auto &x : indexer.searchMatching("txt"))
+	for (const auto &x : indexer.searchMatching("cpp"))
 	{
-		ciof::print(x);
+		ciof::print("FILE NAME: %1 -- %2", x, __timeToStr(indexer.getFileDTInfo(x)));
 	}
 
 	indexer.findIndex("current-dir");
